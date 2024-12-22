3983e538d39364eb613519c4b2d0f9009b5365bf
minglu_ge <geminglu199698@outlook.com>
2024-12-22 20:53:30 +0800

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,15 @@ import pluginPrettier from "eslint-plugin-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  { files: ["**/*.{js,mjs,cjs,ts,vue}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+  {
+    files: ["**/*.vue"],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+  },
   {
     plugins: {
       prettier: pluginPrettier,
@@ -17,14 +26,5 @@ export default [
       "no-debugger": "error",
       "@typescript-eslint/no-explicit-any": "off",
     },
-  },
-  { files: ["**/*.{js,mjs,cjs,ts,vue}"] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
-  {
-    files: ["**/*.vue"],
-    languageOptions: { parserOptions: { parser: tseslint.parser } },
   },
 ];
